40f3f4ab3de86f6c02deba45f91ae0717e123712
sominlee <sayyo1120@gmail.com>
2016-06-03 23:03:35 +0900

    record path edit

--- PreHands_PPTF5_Effect/Dependencies/KinectWpfViewers/Record/RecordController.cs
+++ PreHands_PPTF5_Effect/Dependencies/KinectWpfViewers/Record/RecordController.cs
@@ -41,8 +41,11 @@ namespace Microsoft.Samples.Kinect.WpfViewers
                 if (openBool)
                 {
                     String fileName = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".avi";
+                    String filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos);
+                    String file = filePath + "//" + fileName;
+
                     openBool = false;
-                    writer.Open(fileName, colorImage.Width, colorImage.Height, 25, VideoCodec.MPEG4);
+                    writer.Open(file, colorImage.Width, colorImage.Height, 25, VideoCodec.MPEG4);
                 }
 
                 writer.WriteVideoFrame(bmap);
